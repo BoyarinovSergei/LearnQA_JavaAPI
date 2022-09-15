64c9e3fae5396d351cd1c3b6258337f667514412
Sergei Boyarinov <s.boyarinov@telecontact.ru>
2022-09-15 21:13:40 +0300

    Ex6:* Редирект

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,14 +1,17 @@
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
     public void testGetStatusCode(){
-        JsonPath response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_json_homework")
-                .jsonPath();
-        System.out.println(response.get("messages[1].message").toString());
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(true)
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        System.out.println(response.getHeader("X-Host"));
     }
 }
