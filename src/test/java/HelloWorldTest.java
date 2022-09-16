0ecd809e06665367f98eddad2a6108c9b052a412
Sergei Boyarinov <s.boyarinov@telecontact.ru>
2022-09-16 20:45:01 +0300

    Ex8*: Токены

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,10 +1,16 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.collection.IsMapContaining.hasKey;
 
 public class HelloWorldTest {
 
     @Test
+    @Disabled
     public void testGetStatusCode(){
         Response response;
         String code = "301";
@@ -20,5 +26,35 @@ public class HelloWorldTest {
             code = String.valueOf(response.getStatusCode());
             url = response.getHeader("Location");
         }
+    }
+
+    @Test
+    public void extractToken(){
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+        String token = response.getJsonObject("token").toString();
+
+        JsonPath response2 = RestAssured
+                .given()
+                .param("token",token)
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+        Assertions.assertEquals(response2.getJsonObject(
+                "status").toString(), "Job is NOT ready");
+
+        try {Thread.sleep(Long.parseLong(String.format("%d000", (int) response.getJsonObject("seconds"))));
+        } catch (InterruptedException e) {throw new RuntimeException(e);}
+
+        JsonPath response3 = RestAssured
+                .given()
+                .param("token",token)
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .then()
+                .body("$", hasKey("result"))
+                .and().extract().jsonPath();
+        Assertions.assertEquals(response3.getJsonObject(
+                "status").toString(), "Job is ready");
     }
 }
